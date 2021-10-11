f75f48cbf70a14fde53bf195e6f9721169721c30
ivsorokin <ivan.sorokin@altenar.com>
2021-10-11 20:51:50 +0300

    Task1

---
+++ Task1/Final/Task1/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+            var target = 11;
+            var result = TwoSum(nums, target);
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[2];
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    result[0] = dict.GetValueOrDefault(target - nums[i]);
+                    result[1] = i;
+                    break;
+                }
+                dict.TryAdd(nums[i], i);
+            }
+            return result;
+        }
+    }
+}
---
+++ Task1/First/Task1/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+            var target = 11;
+            var result = TwoSum(nums, target);
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                }
+                if (result[0] != 0 || result[1] != 0)
+                    break;
+            }
+            return result;
+        }
+    }
+}
