388194529a06c96ef76afac9870ce377e62cf89e
ivsorokin <ivan.sorokin@altenar.com>
2021-10-14 09:41:11 +0300

    Task2

---
+++ Task2/LongestSubstringSolution1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = LengthOfLongestSubstring("abba");
+            Console.WriteLine(result);
+        }
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            string test = "";
+            int maxLength = -1;
+            if (String.IsNullOrEmpty(s))
+                return 0;
+            if (s.Length == 1)
+                return 1;
+            foreach (var c in s)
+            {
+                string currentChar = c.ToString();
+                if (test.Contains(currentChar))
+                    test = test.Substring(test.IndexOf(currentChar) + 1);
+                test = test + currentChar;
+                maxLength = Math.Max(test.Length, maxLength);
+            }
+            return maxLength;
+        }
+    }
+}
---
+++ Task2/LongestSubstringSolution2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = LengthOfLongestSubstring("abba");
+            Console.WriteLine(result);
+        }
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            var length = s.Length;
+            if (length == 0)
+                return 0;
+            if (length == 1)
+                return 1;
+            int target = 0;
+            int currentMaxLength;
+            int maxLength = 0;
+            int i;
+            Dictionary<char, int> dict = new Dictionary<char, int>() { [s[0]] = 0 };
+
+            for (i = 1; i < length; i++)
+            {
+                if (!dict.TryAdd(s[i], i))
+                {
+                    if (dict[s[i]] >= target)
+                    {
+                        currentMaxLength = i - target;
+                        if (maxLength < currentMaxLength)
+                        {
+                            maxLength = currentMaxLength;
+                        }
+                        target = dict[s[i]] + 1;
+                    }
+                    dict[s[i]] = i;
+                }
+            }
+            if (maxLength < i - target)
+            {
+                maxLength = i - target;
+            }
+            return maxLength;
+        }
+    }
+}
